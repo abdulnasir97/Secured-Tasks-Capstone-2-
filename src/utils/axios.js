b7b317697607f760a626537e137f39c1de908d77
Abdul M Nasir <moeid.nasir@gmail.com>
2023-08-15 10:17:30 -0400

    new changes

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const backendUrl=process.env.BACKEND_URL;
+const backendUrl=process.env.NEXT_PUBLIC_BACKEND_URL;
 console.log("Backend URL "  , backendUrl);
 
 const axiosInstance = axios.create({
