b165023406ecfcb475822e7d877dc8cdbd35a660
Abdul M Nasir <moeid.nasir@gmail.com>
2023-08-16 14:45:52 -0400

    Update axios.js

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,8 +1,12 @@
 import axios from 'axios';
 
+// Define the URL for the backend server.
 const backendUrl= "https://todo-server-123-4f16fd2da272.herokuapp.com"
+
+// Log the backend URL to the console for reference.
 console.log("Backend URL "  , backendUrl);
 
+// Create an instance of the 'axios' library with custom settings.
 const axiosInstance = axios.create({
   baseURL: backendUrl,
   withCredentials: true,
